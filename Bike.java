e52e5193f57b6b413e61f586d534a7f62f4c02f0
Harshitha736 <harshuacharya2004@gmail.com>
2026-08-12 09:23:24 +0530

    Bike

---
+++ Bike.java
@@ -0,0 +1,25 @@
+ class Bike {
+    public static void main(String[] args) {
+        String brand = "ABC";
+        String model = "X1";
+        String color = "Black";
+        int engineCC = 125;
+        double price = 1000.0;
+        int mileage = 60;
+        String fuelType = "Petrol";
+        int gears = 5;
+        boolean electricStart = true;
+        String owner = "Ravi";
+
+        System.out.println("brand: " + brand);
+        System.out.println("model: " + model);
+        System.out.println("color: " + color);
+        System.out.println("engineCC: " + engineCC);
+        System.out.println("price: " + price);
+        System.out.println("mileage: " + mileage);
+        System.out.println("fuelType: " + fuelType);
+        System.out.println("gears: " + gears);
+        System.out.println("electricStart: " + electricStart);
+        System.out.println("owner: " + owner);
+    }
+}
