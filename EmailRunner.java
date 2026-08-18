025dc3ee09cc3ea18296f8d17d1c65dbfe2c2225
Harshitha736 <harshuacharya2004@gmail.com>
2026-08-18 22:32:19 +0530

    added nem EmailRunner.java

---
+++ EmailRunner.java
@@ -0,0 +1,23 @@
+class EmailRunner{
+	public static void main(String[] args){
+		System.out.println("EmailRunner is in main method");
+		
+		String Email1="harshu@gmail.com";
+		String Email2="anku@gmail.com";
+		String Email3="raju@gmail.com";
+		String Email4="vini@gmail.com";
+		String Email5="rani@gmail.com";
+		String Email6="anu@gmail.com";
+		String Email7="afiffa@gmail.com";
+		String Email8="sneha@gmail.com";
+		String Email9="lalitha@gmail.com";
+		String Email10="aju@gmail.com";
+	
+		String[] emails={Email1,Email2,Email5,Email3,Email4,"Email80",
+		"Email20","Email10","Email28","Email57","Email00"};
+		int totalEmails = emails.length;
+		System.out.println("totalEmails="+totalEmails);
+		
+		
+	}
+}
